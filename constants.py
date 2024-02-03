Fixed gradient descent and plotzz

constants.py
@@ -2,11 +2,11 @@ import numpy as np
 import jax.numpy as jnp
 
 # Epoch and timesteps
-epochs = 100
-timesteps = 1000
+epochs = 10
+timesteps = 20
 
 
-# Loss function - MSE
+# Loss function
 def MSE(model_function, X, w, y_true):
     y_pred = model_function(X, w)
     return jnp.mean((y_pred - y_true) ** 2)
@@ -15,15 +15,23 @@ def MSE(model_function, X, w, y_true):
 # Controller functions and parameters
 # Generic Model of a standard 3-parameter PID Controller
 learning_rate = 0.001
-classic_init_param = 0.1, 0.01, 0.001
+classic_init_param = jnp.asarray([0.1, 0.01, 0.001])
+noise_range = (-0.01, 0.01)
 
 
 def classic_function(X, w):
     error_history = X
     k_p, k_i, k_d = w
-    dE_dt = error_history[-1] - error_history[-2]
+
+    if len(X) <= 2:
+        dE_dt = error_history[-1] - error_history[-2]
+    else:
+        dE_dt = error_history[-1]
+
     E = error_history[-1]
-    sum_E = jnp.sum(error_history)
+    sum_E = sum(error_history)
+
+    # Function
     U = k_p * E + k_d * dE_dt + k_i * sum_E
     return U
 
@@ -32,21 +40,23 @@ def classic_function(X, w):
 # TODO: Implement neural net controller function
 
 # Bathtub constants
-H_0 = 10
+H_0_bathtub = 10
 A = 10
 c = 100
 C = A / c
 g = 9.81
-T_bathtub = 0
+T_bathtub = H_0_bathtub
+error_init = 4
 
 
 # Bathtub input functions and variables
 def bathtub_function(U, D, state):
-    new_state = (U + D - C * np.sqrt(2 * g * state)) / A
+    new_state = state + (U + D - C * np.sqrt(2 * g * state)) / A
     return new_state
 
 
 # Cournot constants
+H_0_cournot = 0
 p_max = 200
 c_m = 1 / 20
 T_cournot = 400

consys.py
@@ -1,6 +1,8 @@
 from jax import jit, value_and_grad
 
 from helpers import noise
+import matplotlib.pyplot as plt
+import jax.numpy as jnp
 
 
 class Consys:
@@ -16,7 +18,7 @@ class Consys:
         self.timesteps = None
 
     def reset(self):
-        self.error_history = []
+        self.error_history = jnp.array([])
         self.params_history = []
         self.mse_history = []
         self.controller.reset_parameters()
@@ -26,39 +28,39 @@ class Consys:
         self.controller.reset_error_history()
         self.plant.reset_state()
 
-    def simulate(self, epochs, timesteps):
+    def simulate(self, epochs, timesteps, noise_range, error_init):
         self.epochs = epochs
         self.timesteps = timesteps
 
         # Reset controller parameters
-        self.controller.reset_parameters()
+        self.reset()
 
         # Initialize gradient function with jax
-        gradient_function = jit(value_and_grad(self.loss_function, argnums=2))
+        gradient_function = value_and_grad(self.loss_function, argnums=2)
 
         for epoch in range(self.epochs):
             # Reset other controller variables and plant state
             self.reset_controller_plant()
 
             # Generate noise
-            D = noise()
+            D = noise(noise_range, self.timesteps)
 
-            error = None
+            error = 0
             # TODO: How to handle first error
 
-            for timestep in range(self.timesteps):
+            for timestep, D_t in zip(range(self.timesteps), D):
                 # Update controller and plant
                 U = self.controller.update(error)
-                Y = self.plant.update(U, D)
+                Y = self.plant.update(U, D_t)
                 error = self.T - Y
 
                 # Record error in history
-                self.error_history.append(error)
+                self.error_history = jnp.append(self.error_history, error)
 
-            # Declare som variables for readability
+            # Declare some variables for readability
             model_function = self.controller.controller_function
             X = self.error_history
-            w = self.controller.controller_params
+            w = self.controller.parameters
             y_true = self.T
 
             # Compute MSE
@@ -70,8 +72,23 @@ class Consys:
 
             # Update controller parameters using gradient descent
             self.controller.update_params(gradients)
-            self.params_history.append(self.controller.params)
-
+            self.params_history.append(self.controller.parameters)
+            print(self.params_history)
             # TODO: Check if correct MSE computing and gradient descent
 
-        return U, Y, self.mse_history, self.params_history
+        return U, Y
+
+    def print_mse_history(self):
+        plt.plot(self.mse_history)
+        plt.xlabel('Epoch')
+        plt.ylabel('MSE')
+        plt.title('Learning Progression')
+        plt.legend()
+        plt.show()
+
+    def print_parameter_history(self):
+        plt.plot(self.params_history)
+        plt.xlabel('Epoch')
+        plt.ylabel('Y')
+        plt.title('Control Parameters')
+        plt.show()

controller.py
@@ -1,5 +1,4 @@
-import jax
-import numpy as np
+import jax.numpy as jnp
 
 
 class Controller:
@@ -8,18 +7,18 @@ class Controller:
         self.initial_parameters = initial_parameters
         self.parameters = initial_parameters
         self.learning_rate = learning_rate
-        self.error_history = []
+        self.error_history = jnp.array([])
 
     def reset_parameters(self):
         self.parameters = self.initial_parameters
 
     def reset_error_history(self):
-        self.error_history = []
+        self.error_history = jnp.array([])
 
     def update_params(self, gradients):
-        self.parameters -= self.learning_rate * gradients
+        self.parameters -= jnp.multiply(self.learning_rate, gradients)
 
     def update(self, error):
-        self.error_history.append(error)
+        self.error_history = jnp.append(self.error_history, error)
         U = self.controller_function(self.error_history, self.parameters)
         return U

helpers.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 
-def noise():
-    return np.random
-
-# TODO: Implement range of noise, the range should be configured in constants
+def noise(noise_range, noise_dim):
+    lower_bound, upper_bound = noise_range
+    noise = np.random.uniform(low=lower_bound, high=upper_bound, size=noise_dim)
+    return noise